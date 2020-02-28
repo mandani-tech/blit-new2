8b7d367d137e4aed8465aee9962ef4bc56bce8f3
student <mandani.hamida@gmail>
2020-02-28 11:11:23 -0600

    raygreen code added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hamida Mandani
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: [],
+    };
+  }
+
+  render() {
+    return (
+        <div className="container">
+          <section className="hero">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  BLiT Businesses
+                </h1>
+                <h2 className="subtitle">
+                  by Ray
+                </h2>
+              </div>
+            </div>
+          </section>
+          <hr/>
+          {this.state.businesses.map(business =>
+              <div className="card" key={business.objectID}>
+                <div className="card-content">
+                  <p className="title">
+                    {business['name']}
+                  </p>
+                  <p className="subtitle">
+                    {business['owner']}
+                  </p>
+                </div>
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+            <span>
+            <a href="{business.url}">View Website</a>
+            </span>
+                  </p>
+                </footer>
+              </div>
+          )}
+        </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch('https://blacklatinoit.com/api/businesses')
+        .then(response => response.json())
+        .then(data => {
+          var result = Object.keys(data).map((key) => {
+            return data[key];
+          });
+          console.log(result);
+          this.setState({ businesses: result })
+        });
+  }
 }
 
 export default App;
